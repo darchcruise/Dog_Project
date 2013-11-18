dfca9e06da68c9f71ddb619fa62118339ec1232a
Jason Darcy <darchcruise@Jasons-iMac.local>
2013-11-18 11:46:41 -0500

    Dog Homepage and Service page

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,7 @@
+class PetsController < ApplicationController
+  def first
+  end
+
+  def second
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bs1::Application.config.secret_token = 'b2b26e260d39be7496c93dbdc38eb24c983fdcd992c73601543e5aea64ad90bc6773ce9e892628ddc5649522848fe68e7ed426b178e3f75a32e8456d5ec689ab'
